0f99dd4627a3832e73064a81b1d9827367990b26
SMJin <sstaw@naver.com>
2025-03-23 20:05:27 +0900

    creating environment

---
+++ src/main/java/jinno/live_voting_service/LiveVotingServiceApplication.java
@@ -0,0 +1,13 @@
+package jinno.live_voting_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveVotingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveVotingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jinno/live_voting_service/LiveVotingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package jinno.live_voting_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveVotingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
